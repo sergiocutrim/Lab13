4a60d6b34993ef426188e0c4e390e36b3bd885ba
Sergio Gouveia <97315931+sergiocutrim@users.noreply.github.com>
2024-04-05 12:14:32 -0400

    Add files via upload

---
+++ Lab13/includes/footer-nav.php
@@ -0,0 +1,7 @@
+			<!-- Fine print navigation -->
+			<nav>
+				<a href="/sergio.cutrim/Lab13/about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | 
+				<a href="/sergio.cutrim/Lab13/about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> | 
+				<a href="/sergio.cutrim/Lab13/about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a> | 
+				<a href="/sergio.cutrim/Lab13/about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
+			</nav>
---
+++ Lab13/includes/global-nav.php
@@ -0,0 +1,22 @@
+			<!-- Global site navigation -->
+			<!-- STEP 1: Add a nested UL and LI elements for the two pages inside the "About" section, privacy-policy.php and terms-of-use.php -->
+			<nav>
+				<ul>
+					<li><a href="/sergio.cutrim/Lab13/index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="/sergio.cutrim/Lab13//red.php" title="Learn about red blocks">Red Blocks</a>
+						<ul>
+							<li><a href="/sergio.cutrim/Lab13/red/gallery.php" title="Browse our other types of red blocks">Red Blocks Gallery</a></li>
+						</ul>
+					</li>
+					<li><a href="/sergio.cutrim/Lab13/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="/sergio.cutrim/Lab13/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="/sergio.cutrim/Lab13/green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<li><a href="/sergio.cutrim/Lab13/about.php" title="Learn more about us">About</a>
+						<ul>
+							<li><a href="/sergio.cutrim/Lab13/about/privacy-policy.php" title="Learn about our privacy policy">Privacy Policy</a></li>
+							<li><a href="/sergio.cutrim/Lab13/about/terms-of-use.php" title="Learn about our terms of use">Term of Use</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			<!-- STEP 2: Upload this file to your Web server for your "Building Blocks" site to see the result, then head to 'styles.css' for STEP 3 -->
